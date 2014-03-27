fc95431ad48c102820bbb1f263b1930e981cb075
aguadopd <aguadopd@hotmail.com>
2014-03-27 18:35:44 -0300

    Comienzo a escribir el programa principal.

---
+++ codigo/detecciondepersonas_opencv.cpp
@@ -0,0 +1,8 @@
+//============================================================================
+// Name        : detecciondepersonas_opencv.cpp
+// Author      : Fabricio Emder - Pablo Aguad
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
