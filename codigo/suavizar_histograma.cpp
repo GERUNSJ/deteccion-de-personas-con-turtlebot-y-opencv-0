2c6125d6abe9cd1009c740df5b80dedd90e285e2
aguadopd <aguadopd@hotmail.com>
2014-05-01 11:37:47 -0300

    añadidos los archivos de suavizar_histograma.

---
+++ codigo/suavizar_histograma.cpp
@@ -0,0 +1,10 @@
+/*
+ * suavizar_histograma.cpp
+ *
+ *  Created on: 01/05/2014
+ *      Author: pablo
+ */
+
+
+
+
---
+++ codigo/suavizar_histograma.hpp
@@ -0,0 +1,15 @@
+/*
+ * suavizar_histograma.hpp
+ *
+ *  Created on: 01/05/2014
+ *      Author: pablo
+ */
+
+#ifndef SUAVIZAR_HISTOGRAMA_HPP_
+#define SUAVIZAR_HISTOGRAMA_HPP_
+
+
+#include <suavizar_histograma.cpp>
+
+
+#endif /* SUAVIZAR_HISTOGRAMA_HPP_ */
