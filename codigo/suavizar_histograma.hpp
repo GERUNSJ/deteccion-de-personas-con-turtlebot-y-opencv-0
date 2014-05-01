0452f229502953ec69b078f8358301db9bc0e654
aguadopd <aguadopd@hotmail.com>
2014-05-01 12:07:15 -0300

    No andaba con hpp y cpp

--- codigo/suavizar_histograma.hpp
+++ codigo/suavizar_histograma.hpp
@@ -8,8 +8,21 @@
 #ifndef SUAVIZAR_HISTOGRAMA_HPP_
 #define SUAVIZAR_HISTOGRAMA_HPP_
 
+using namespace cv;
+using namespace std;
 
-#include <suavizar_histograma.cpp>
+//#include <suavizar_histograma.cpp>
+
+void suavizar_histograma(Mat& histograma);
+
+
+
+void suavizar_histograma(Mat& histograma)
+{
+	//C++: void blur(InputArray src, OutputArray dst, Size ksize, Point anchor=Point(-1,-1), int border-type=BORDER_DEFAULT )
+	blur(histograma, histograma, Size(1,5), Point(-1,-1), BORDER_REPLICATE);	//Size(columnas,filas)
+}
+
 
 
 #endif /* SUAVIZAR_HISTOGRAMA_HPP_ */
