8fb7bafc0f90457362957c76a78113564deda9f4
aguadopd <aguadopd@hotmail.com>
2014-05-15 13:30:35 -0300

    FUNCIONA. Devuelve un solo vector.

--- codigo/valles.hpp
+++ codigo/valles.hpp
@@ -6,6 +6,45 @@
  */
 
 #ifndef VALLES_HPP_
+
+//Para 8 bits.
+std::vector<int> encontrar_valles(cv::Mat& histograma)
+{
+	std::vector<int> valles;
+	std::vector<int>::iterator it;
+	int i = 0;
+
+	//Primer elemento
+	valles.push_back(0);
+
+	for(i=1; i < histograma.rows-1 ; i++)	//Del 1 al 254
+	{
+
+		if(	(histograma.at<float>(i) <= histograma.at<float>(i-1)) &&
+			(histograma.at<float>(i) < histograma.at<float>(i+1)) )
+			{
+				//std::cout << "\n" << histograma.at<float>(i-1) << " > " << histograma.at<float>(i) << " < " << histograma.at<float>(i+1);
+				valles.push_back(i);
+			}
+	}
+
+	//Último elemento
+	valles.push_back(255);
+
+
+	for(it = valles.begin(); it != valles.end() ; it++)
+	{
+		std::cout << ' ' << *it;
+	}
+	std::cout << '\n';
+
+	std::cout << "\n\nHay " << valles.size() << " minimos.\n";
+
+	return valles;
+}
+
+
+
 #define VALLES_HPP_
 
 
